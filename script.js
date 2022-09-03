fa3d6eb748c033aa032197005b4f5eed57f5b969
presh-031 <akingbulugbe.dev@gmail.com>
2022-09-03 07:39:30 +0100

    bill, tip, custom tip and numberofpeople values are ready

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
 'use strict';
+getTipPercentage();
+getCustomTip();
+getNumOfPeople();
+getBill();
+
+function getTipPercentage() {
+  const possibleTips = document.querySelectorAll('.tips-percentage');
+  let selectedTip = '';
+
+  for (let i = 0; i < possibleTips.length; i++) {
+    possibleTips[i].addEventListener('click', e => {
+      selectedTip = e.target.innerHTML;
+      console.log(selectedTip);
+    });
+  }
+}
+function getCustomTip() {
+  let customTipInput = document.querySelector('.custom');
+  let customTip = '';
+  customTipInput.addEventListener('change', () => {
+    customTip = customTipInput.value;
+    console.log(customTip);
+  });
+}
+
+function getNumOfPeople() {
+  const numOfPeopleInput = document.querySelector('#num-of-people');
+  let numOfPeople = '';
+  numOfPeopleInput.addEventListener('change', () => {
+    numOfPeople = numOfPeopleInput.value;
+    console.log(numOfPeople);
+  });
+}
+
+function getBill() {
+  const billInput = document.querySelector('#bill');
+  let bill = '';
+  billInput.addEventListener('change', () => {
+    bill = billInput.value;
+    console.log(bill);
+  });
+}
