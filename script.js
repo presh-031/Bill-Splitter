0756082852789e695e71f6053d2ab638b38979f9
presh-031 <akingbulugbe.dev@gmail.com>
2022-09-14 19:39:52 +0100

    still debugging buttons

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const numOfPeopleInput = document.querySelector('#num-of-people');
 const billInput = document.querySelector('#bill');
 const tipsPercentageBtns = document.querySelectorAll('.tips-percentage');
 
+let fixedTip;
+
 function getBillValue() {
   const bill = billInput.value;
   return +bill;
@@ -50,32 +52,50 @@ billInput.addEventListener('input', () => {
   callCalculateWithCustomTip();
   callCalculateWithFixedTip();
 });
-// Tips percentage btns
+// //Tips percentage btns
 // Get fixed tip percentage
-tipsPercentageBtns.forEach(btn => {
-  btn.addEventListener('click', () => {
-    callCalculateWithFixedTip(btn.innerHTML.slice(0, -1));
+// let btnClicked;
+// tipsPercentageBtns.forEach(btn => {
+//   btn.addEventListener('mousedown', () => {
+//     btnClicked = btn.innerHTML.slice(0, -1);
+//     callCalculateWithFixedTip(btnClicked);
 
-    // one-liner solving the 'pressing on button at a time styles' issue.
-    tipsPercentageBtns.forEach(btn => btn.classList.remove('active'));
-    btn.classList.add('active');
-  });
-});
+//     // one-liner solving the 'pressing on button at a time styles' issue.
+//     tipsPercentageBtns.forEach(btn => btn.classList.remove('active'));
+//     btn.classList.add('active');
+//   });
+// });
+// let btns = [];
 // for (let i = 0; i < tipsPercentageBtns.length; i++) {
 //   tipsPercentageBtns[i].addEventListener('click', () => {
-//     let btn = tipsPercentageBtns[i];
-//     console.log(btn);
-
-//     if (btn.click) {
-//       console.log(btn.innerHTML);
-//       btn.style.backgroundColor = 'red';
-//     } else {
-//       btn.style.backgroundColor = 'blue';
-//     }
-//     console.log(`${btn}`);
+//     customTipInput.value = '';
+//     // console.log(tipsPercentageBtns[i].innerHTML.slice(0, -1));
+//     btns.push(tipsPercentageBtns[i].innerHTML.slice(0, -1));
+//     // console.log(btns[btns.length - 1]);
+//     called();
 //   });
-//   tipsPercentageBtns[i].style.backgroundColor = 'blue';
 // }
+
+tipsPercentageBtns.forEach(btn => {
+  btn.addEventListener('click', handleClick);
+});
+
+function handleClick(event) {
+  tipsPercentageBtns.forEach(btn => {
+    btn.classList.remove('active');
+    if (event.target.innerHTML === btn.innerHTML) {
+      btn.classList.add('active');
+      fixedTip = parseFloat(btn.innerHTML);
+      // console.log(buttonVal)
+      // making the innerHTML of a button to be a string,
+      // then divide by 100 to make a percent because parsefloat cannot read % in integer.
+      console.log(fixedTip);
+    }
+  });
+  customTipInput.value = '';
+  callCalculateWithFixedTip(fixedTip);
+}
+
 // customtip-input
 customTipInput.addEventListener('input', () => {
   callCalculateWithCustomTip();
@@ -85,3 +105,11 @@ numOfPeopleInput.addEventListener('input', () => {
   callCalculateWithCustomTip();
   callCalculateWithFixedTip();
 });
+
+// let trial = 0;
+
+// function setTrial() {
+//   trial = 1;
+// }
+// setTrial();
+// console.log(trial);
